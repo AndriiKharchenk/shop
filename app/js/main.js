0c34f3f14c2d0d6cb29ba39567f260e74df90a06
AndriiKharchenk <levsha90@gmail.com>
2025-09-24 08:15:00 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
+const swiper = new Swiper('.accessories__slider', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.accessories__arrow-prev',
+    prevEl: '.accessories__arrow-next',
+  },
+});
